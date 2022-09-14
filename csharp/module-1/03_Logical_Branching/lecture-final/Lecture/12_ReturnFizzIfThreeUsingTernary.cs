07c1a136a0fb10a0d64c0c5e7fbe6ee1eefeb9f5
Tori Brenneison <tori@techelevator.com>
2022-09-14 11:56:31 -0400

    day 03 lecture-final

---
+++ csharp/module-1/03_Logical_Branching/lecture-final/Lecture/12_ReturnFizzIfThreeUsingTernary.cs
@@ -0,0 +1,21 @@
+﻿namespace Lecture
+{
+    public partial class LectureExample
+    {
+        /*
+11. Write an if statement that returns "Fizz"
+    if the parameter is 3 and returns an empty string for anything else.
+    TOPIC: Conditional Logic
+*/
+
+        /*
+        12. Now write the above using the Ternary operator ?:
+        */
+        public string ReturnFizzIfThreeUsingTernary(int number)
+        {
+            return (number == 3) ? "Fizz" : "";
+        }
+
+        //Write the code that makes sense to you -- if you need to solve with if/else, use that, if you like ternaries, use those
+    }
+}
---
+++ csharp/module-1/03_Logical_Branching/lecture-final/Lecture/16_ReturnTrueWhenLargeEvenNumber.cs
@@ -0,0 +1,29 @@
+﻿namespace Lecture
+{
+    public partial class LectureExample
+    {
+        /*
+        16. Return "Big Even Number" when number is even, larger than 100, and a multiple of 5.
+            Return "Big Number" if the number is just larger than 100.
+            Return empty string for everything else.
+            TOPIC: Complex Expression
+        */
+        public string ReturnBigEvenNumber(int number)
+        {
+            if (number % 2 == 0 && number > 100 && number % 5 == 0) //number divided by two has zero remainder if even, and number is over 100, and number divided by five has zero remainder
+            {
+                return "Big Even Number";
+            }
+            else if (number > 100) //if line 13 fails, check if the number is over 100 specifically
+            {
+                return "Big Number";
+            }
+            return "";
+
+
+
+
+
+        }
+    }
+}
