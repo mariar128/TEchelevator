b4d66acefb13f968a2f81a9e71d18b7d5308f011
Tori Brenneison <tori@techelevator.com>
2022-09-15 12:23:29 -0400

    day 4 lecture and exercise, day 5 tutorial

---
+++ csharp/module-1/04_Loops_and_Arrays/lecture-final/Lecture/06_ReturnSumArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lecture
+{
+    public partial class LectureProblem
+    {
+        /*
+        6. How can we write a for loop that sums up the values in this array?
+           TOPIC: For Loops
+        */
+        public int ReturnSumArray()
+        {
+            int[] arrayToLoopThrough = { 3, 4, 2, 9 };
+
+            int sum = 0; //scope
+                         //sum starts out life as 0 
+
+            for(int i = 0; i < arrayToLoopThrough.Length; i++)
+            {
+                sum = sum + arrayToLoopThrough[i]; //changes the value of sum by adding the current value of sum + what is at the specified array index
+            }
+
+            return sum;
+        }
+    }
+}
