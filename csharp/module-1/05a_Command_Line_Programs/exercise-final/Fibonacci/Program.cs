72a56b84b8db28491ee0891140b57a854c783e8e
David Ferreira <david.ferreira@techelevator.com>
2022-09-22 14:08:42 -0400

    Finals week 1

---
+++ csharp/module-1/05a_Command_Line_Programs/exercise-final/Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            int limit = int.Parse(Console.ReadLine());
+            int previous = 1;
+
+            Console.Write("0, 1");
+
+            for (int next = 1; next <= limit;)
+            {
+                Console.Write(", " + next);
+
+                int temp = previous + next;
+                previous = next;
+                next = temp;
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
