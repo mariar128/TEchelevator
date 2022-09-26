8c02ad8b3793606c5f1b1cc654b42383a037ed77
David Ferreira <david.ferreira@techelevator.com>
2022-09-26 13:46:26 -0400

    Day 11 lecture final and Day 12.

---
+++ csharp/module-1/11_Inheritance/lecture-final/InheritanceLecture/Auctioneering/Auction.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InheritanceLecture.Auctioneering
+{
+    /// <summary>
+    /// This class represents a general auction.
+    /// </summary>
+    public class Auction
+    {
+        /// <summary>
+        /// This is an encapsulated field that holds all placed bids.
+        /// </summary>
+        private List<Bid> allBids = new List<Bid>();
+
+        /// <summary>
+        /// Represents the current high bid.
+        /// </summary>
+        public Bid CurrentHighBid { get; private set; } = new Bid("", 0);
+
+        /// <summary>
+        /// All placed bids returned as an array.
+        /// </summary>
+        public Bid[] AllBids
+        {
+            get { return allBids.ToArray(); }
+        }
+
+        /// <summary>
+        /// Indicates if the auction has ended yet.
+        /// </summary>
+        public bool HasEnded { get; private set; }
+
+        /// <summary>
+        /// Places a Bid on the Auction
+        /// </summary>
+        /// <param name="offeredBid">The bid to place.</param>
+        /// <returns>True if the new bid is the current winning bid</returns>
+        public virtual bool PlaceBid(Bid offeredBid)
+        {
+            //Only accept bids if the auction has not ended.
+
+
+            bool newHighBidPlaced = false;
+            // Print out the bid details.
+            //biddername + ammount in currency format. 
+            Console.WriteLine(offeredBid.Bidder + " bid " + offeredBid.BidAmount.ToString("C"));
+
+            //!HasEnded
+            if (HasEnded == false)
+            {
+                // Record it as a bid by adding it to allBids
+                allBids.Add(offeredBid);
+
+                // Check to see IF the offered bid is higher than the current bid amount
+                if (offeredBid.BidAmount > CurrentHighBid.BidAmount)
+                {
+                    // if yes, set offered bid as the current high bid
+                    CurrentHighBid = offeredBid;
+                    newHighBidPlaced = true;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Bid not accepted - Auction has ended.");
+            }
+            // Output the current high bid
+            Console.WriteLine($"Current highest bid {CurrentHighBid.BidAmount.ToString("C")} by {CurrentHighBid.Bidder}");
+            // Return if this is the new highest bid
+            return newHighBidPlaced;
+        }
+
+
+       
+        //TODO - Stop accepting bids once auction is ended. 
+        public void EndAuction()
+        {
+            //End the auction
+            HasEnded = true;
+            //Annouce the winner
+            Console.WriteLine($"Auction has finished, {CurrentHighBid.Bidder} has won with a bid of {CurrentHighBid.BidAmount.ToString("C")}.");
+        }
+
+    }
+}
---
+++ csharp/module-1/11_Inheritance/lecture-final/InheritanceLecture/Program.cs
@@ -0,0 +1,46 @@
+﻿using InheritanceLecture.Auctioneering;
+using System;
+
+namespace InheritanceLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new general auction
+            Console.WriteLine("Starting a general auction");
+            Console.WriteLine("-----------------");
+
+            Auction generalAuction = new Auction();
+
+            generalAuction.PlaceBid(new Bid("Josh", 1));
+            generalAuction.PlaceBid(new Bid("Fonz", 23));
+            generalAuction.PlaceBid(new Bid("Rick Astley", 13));
+            //....
+            //....
+            // This might go on until the auction runs out of time or hits a max # of bids
+            generalAuction.PlaceBid(new Bid("David", 25));
+            generalAuction.PlaceBid(new Bid("Ben", 50));
+            generalAuction.EndAuction();
+            generalAuction.PlaceBid(new Bid("David", 51));
+
+            // Buy out 
+            Console.WriteLine("Starting a Buyout auction");
+            Console.WriteLine("-----------------");
+            BuyoutAuction buyoutAuction = new BuyoutAuction(1000);
+            buyoutAuction.PlaceBid(new Bid("David", 5));
+            buyoutAuction.PlaceBid(new Bid("Taylor", 1000));
+            Console.WriteLine(buyoutAuction.CurrentHighBid.Bidder);
+
+            // Reserve 
+            ReserveAuction reserveAuction = new ReserveAuction(200);
+            reserveAuction.PlaceBid(new Bid("David", 150));
+            reserveAuction.PlaceBid(new Bid("Kate", 201));
+            reserveAuction.PlaceBid(new Bid("Ben", 2000000));
+            reserveAuction.EndAuction();
+
+
+            Console.ReadLine();
+        }
+    }
+}
