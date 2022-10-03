4fc40a30b7e1403d4a07544015fb5978753ef0da
Tori Brenneison <tori@techelevator.com>
2022-10-03 11:51:59 -0400

    file IO read & ex. handling exercise and lecture-final

---
+++ csharp/module-1/16b_File_IO_Reading/lecture-final/Lecture/Program.cs
@@ -0,0 +1,22 @@
+﻿using Lecture.Aids;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileAndDirectories fad = new FileAndDirectories();
+           //fad.UsingTheDirectoryClass();
+
+            //FilePaths filepathz = new FilePaths();
+            //filepathz.UsingPathForTemporaryFolders();
+
+
+            //ReadingInFiles.ReadACharacterFile();
+            //ReadingCSVFiles.ReadFile();
+            //SummingUpNumbers.ReadFile();
+            
+        }
+    }
+}
