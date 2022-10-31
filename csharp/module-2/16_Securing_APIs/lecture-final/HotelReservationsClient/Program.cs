6425ddf23a2589ef3ce9dd6ef1e39a590f36ac99
Tori Brenneison <tori@techelevator.com>
2022-10-31 11:51:35 -0400

    API security exercise and lecture-final

---
+++ csharp/module-2/16_Securing_APIs/lecture-final/HotelReservationsClient/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelReservationsClient
+{
+    class Program
+    {
+        private const string ApiUrl = "https://localhost:5001/";
+        static void Main()
+        {
+            HotelApp app = new HotelApp(ApiUrl);
+            app.Run();
+        }
+    }
+}
---
+++ csharp/module-2/16_Securing_APIs/lecture-final/HotelReservationsClient/Services/AuthenticationApiService.cs
@@ -0,0 +1,39 @@
+﻿using HotelReservationsClient.Models;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace HotelReservationsClient.Services
+{
+    public class AuthenticationApiService : ApiService
+    {
+        private static ApiUser user = new ApiUser();
+        public bool LoggedIn { get { return !string.IsNullOrWhiteSpace(user.Token); } }
+        public AuthenticationApiService(string apiUrl) : base(apiUrl) { }
+
+        public bool Login(string submittedName, string submittedPass)
+        {
+            LoginUser loginUser = new LoginUser { Username = submittedName, Password = submittedPass };
+            RestRequest request = new RestRequest("login");
+            request.AddJsonBody(loginUser);
+            IRestResponse<ApiUser> response = client.Post<ApiUser>(request);
+
+            CheckForError(response, "Login");
+            user.Token = response.Data.Token;
+
+            //Set the token on the client
+            client.Authenticator = new JwtAuthenticator(user.Token);
+            //this will send the logged-in user's token on all subsequent requests 
+
+            return true;
+        }
+
+        public void Logout()
+        {
+            user = new ApiUser();
+
+            //Remove the token from the client
+            client.Authenticator = null;
+
+        }
+    }
+}
