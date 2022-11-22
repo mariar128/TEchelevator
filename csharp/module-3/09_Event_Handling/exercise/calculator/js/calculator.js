470bf781d2be99740a9c1e7e430b482682c0b9de
maria rivera <maria.rivera898@yahoo.com>
2022-11-22 10:21:30 -0500

    javascript === evil

--- csharp/module-3/09_Event_Handling/exercise/calculator/js/calculator.js
+++ csharp/module-3/09_Event_Handling/exercise/calculator/js/calculator.js
@@ -72,29 +72,50 @@ function clear() {
 }
 
 // add event listener for when the DOM is loaded
-document.addEventListener('LOADED_EVENT_GOES_HERE', () => {
+document.addEventListener('DOMContentLoaded', (event) => {
 
-  // set the variable called display declared at the top of this file equal to the display element
+  display = document.getElementById('display');
+
+ // set the variable called display declared at the top of this file equal to the display element
   // HINT: use its id #display to get a reference to it
+
+ 
+    
 
   // get a reference to all of the numbers
   // loop over each of the numbers
   // add a click event listener to each number to call the function clickNumber
+  const calcButtonArray = document.querySelectorAll('.number');
+  calcButtonArray.forEach((calcButton) => {
+      calcButton.addEventListener('click', clickNumber)
+  });
 
-  // get a reference to the decimal point button
+      
+
+// get a reference to the decimal point button
   // add a click event listener to call the function clickNumber
   // the decimal point is part of the number so you can call clickNumber for it 
   // as you would for a number
-
+  const decimalButton = document.querySelector('.decimal')
+  decimalButton.addEventListener('click', clickNumber);
+  
+  
   // get a reference to the all clear button
   // add a click event listener to call the function clear  
+const clearButtonArray = document.querySelector('.all-clear')
+clearButtonArray.addEventListener('click',clear);
+
 
   // get a reference to all of the operators;
   // loop over each of the operators
   // add a click event listener to each operator to call the function clickOperator
+const operatorButtonElement = document.querySelectorAll('.operator');
+operatorButtonElement.forEach((operatorButton) => {
+operatorButton.addEventListener('click', clickOperator)});
 
   // add click event listener for the equal sign
   // should call the function performOperation
-
+const equalOperator = document.querySelector('.equal-sign');
+equalOperator.addEventListener('click', performOperation);
 });
 
--- csharp/module-3/09_Event_Handling/exercise/shopping-list/js/shopping-list.js
+++ csharp/module-3/09_Event_Handling/exercise/shopping-list/js/shopping-list.js
@@ -76,6 +76,7 @@ clickButton.addEventListener('dblclick', event => {
   
   })
   });
+  
 
 
 });
