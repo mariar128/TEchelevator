d2b03b734dcff60513116121c6e6fb16eac75025
Tori Brenneison <tori@techelevator.com>
2022-11-28 12:15:45 -0500

    Vue Router exercise

---
+++ csharp/module-3/15_Vue_Router/lecture-final/src/views/AddReview.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>Add Review</h1>
+      <add-review/>
+  </div>
+</template>
+
+<script>
+import AddReview from '../components/AddReview.vue'
+
+export default {
+    name: 'add-product-review', //don't name the same as the thing you're importing
+    components: { AddReview }
+}
+</script>
+
+<style>
+
+</style>
